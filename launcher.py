279bda9b9b89653778a4be70cc244b5c64b3acdc
HafizWayne <mhafidzdaffa42@gmail.com>
2025-10-08 21:42:17 +0700

    fixing perhitungan_full.py

---
+++ launcher.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL aplikasi Streamlit Anda yang sudah di-deploy
+URL_APLIKASI = "https://indobert-indication-depression.streamlit.app/"
+
+# Buka URL di browser default pengguna
+webbrowser.open(URL_APLIKASI)
